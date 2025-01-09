0f28709c300d415f364ebdd938373922e171b364
swati-zampal <131242062+swati-zampal@users.noreply.github.com>
2025-01-09 13:56:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('changed not ');
+ res.send('jhala re change ');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
