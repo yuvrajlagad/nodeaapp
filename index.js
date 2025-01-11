19536cbe8a0379086c1ffb9010e618cd4d5e2c67
yuvrajlagad <yuvrajlagad1921@gmail.com>
2025-01-11 14:51:01 +0530

    ok

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('nahi  ka re baba mi ');
+ res.send('nahi toh  ka re baba mi ');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
